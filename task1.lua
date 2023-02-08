7b792dc1d48e417198535c4696f5514039b3fd07
Zacharie Lawson <72023377+zachmae@users.noreply.github.com>
2023-02-08 14:08:11 +0100

    [PRESENCE] lawson zacharie

---
+++ task1.lua
@@ -0,0 +1,9 @@
+for i = 1 ,100,1 do
+    if i % 3 == 0 and i % 5 == 0 then
+        print("ThreeFive")
+    elseif i % 3 == 0 then
+        print("Three")
+    elseif i % 5 == 0 then
+        print("Five")
+    end
+end
